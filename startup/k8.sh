c0366582dcef4ef4f24719cb435ce47a31c92b76
Sushrut Shringarputale <contact@sushshring.com>
2019-04-01 09:25:39 -0400

    modified k8 starrtup script

--- startup/k8.sh
+++ startup/k8.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+function deps {
+    apt update
+    apt install -y software-properties-common \
+	    net-tools \
+	    wget \
+	    vim \
+	    git
+}
+
 function configure {
-    wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/arm64/docker-ce_18.06.1~ce~3-0~ubuntu_arm64.deb -o docker.deb
-    add-apt-repository ppa:longsleep/golang-backports
+    wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/arm64/docker-ce_18.06.1~ce~3-0~ubuntu_arm64.deb 
+    add-apt-repository ppa:longsleep/golang-backports -y
     apt update
     apt install -y net-tools \
         build-essential libgcrypt-dev libcurl4-gnutls-dev \
@@ -9,7 +18,7 @@ function configure {
         htop iftop \
         apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
         golang-go
-    dpkg -i docker.deb
+    dpkg -i docker-ce_18.06.1~ce~3-0~ubuntu_arm64.deb
     git clone https://github.com/sushshring/network_experiment.git
     git clone https://github.com/sushshring/kubernetes.git
     apt install -y socat ebtables
@@ -38,5 +47,5 @@ WantedBy=multi-user.target" > /etc/systemd/system/kubelet.service
     sudo systemctl enable kubelet
     kubeadm init
 }
-
+deps
 configure > /home/cfg.tmp.log
