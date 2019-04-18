bb977f037e53f3074447753f0ac41e31377cf825
Sushrut Shringarputale <contact@sushshring.com>
2019-04-18 18:51:10 -0400

    Added data and client works

--- analysis/runner.py
+++ analysis/runner.py
@@ -13,11 +13,11 @@ def run_experiment(server='192.168.1.103', port='6000', orchestrator='k8', flood
     print('Finished running client. Hit enter to continue...')
     sys.stdin.readline()
     analysis_cmd = ['./analyze.py', '--show-rtts', '--show-histogram', '--show-histogram-control',
-                    '--remove-outliers', '--outlier-removal-type', '3', '--full-histogram', './client/client_times',
+                    '--remove-outliers', '--outlier-removal-type', '3', '--full-histogram', '../client/client_times',
                     '%s with %s flooding' % (orchestrator, flooding)]
     proc1 = subprocess.run(analysis_cmd)
     now = datetime.now()
-    mv_cmd = ['mv', '../client/client_times', '../data/client_times_%s_%s_intf_%s%s%s_%s%s' % (
+    mv_cmd = ['mv', '../client/client_times', '../data/client_times_%s_%s_intf_control_%s%s%s_%s%s' % (
         orchestrator, flooding, now.month, now.day, now.year, now.hour, now.second)]
     proc2 = subprocess.run(mv_cmd)
     return 0
@@ -26,10 +26,10 @@ def run_experiment(server='192.168.1.103', port='6000', orchestrator='k8', flood
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the experiment')
-    parser.add_argument('--server', type=str, help='IP address of the server')
-    parser.add_argument('--port', type=str, help='port of the server')
-    parser.add_argument('--orchestrator', type=str, help='Orchestrator type for data and labels')
-    parser.add_argument('--flooding', type=str, help='Flooding level for data and labels')
+    parser.add_argument('--server', type=str, help='IP address of the server', default='192.168.1.103')
+    parser.add_argument('--port', type=str, help='port of the server', default='6000')
+    parser.add_argument('--orchestrator', type=str, help='Orchestrator type for data and labels', default='k8')
+    parser.add_argument('--flooding', type=str, help='Flooding level for data and labels', default='quad')
     args = parser.parse_args()
     print(args)
     while True:
