bab0074aaa4ef9b187e1646ae8c2c176fd9239da
Sushrut Shringarputale <contact@sushshring.com>
2019-04-19 17:36:31 -0400

    Added automator runner script

--- analysis/runner.py
+++ analysis/runner.py
@@ -10,14 +10,10 @@ def run_experiment(server='192.168.1.103', port='6000', orchestrator='k8', flood
     print('Restart the flooder now....')
     client_cmd = ['../client/client', '-v', server, port]
     proc = subprocess.run(client_cmd)
-    print('Finished running client. Hit enter to continue...')
-    sys.stdin.readline()
-    analysis_cmd = ['./analyze.py', '--show-rtts', '--show-histogram', '--show-histogram-control',
-                    '--remove-outliers', '--outlier-removal-type', '3', '--full-histogram', '../client/client_times',
-                    '%s with %s flooding' % (orchestrator, flooding)]
-    proc1 = subprocess.run(analysis_cmd)
+    #analysis_cmd = ['./analyze.py', '--show-rtts', '--show-histogram', '--show-histogram-control', '--remove-outliers', '--outlier-removal-type', '3', '--full-histogram', '../client/client_times', '%s with %s flooding' % (orchestrator, flooding)]
+    #proc1 = subprocess.run(analysis_cmd)
     now = datetime.now()
-    mv_cmd = ['mv', '../client/client_times', '../data/client_times_%s_%s_intf_control_%s%s%s_%s%s' % (
+    mv_cmd = ['mv', '../client/client_times', '../data/client_times_%s_%s_intf_control_%s%s%s_%02d%02d' % (
         orchestrator, flooding, now.month, now.day, now.year, now.hour, now.second)]
     proc2 = subprocess.run(mv_cmd)
     return 0
