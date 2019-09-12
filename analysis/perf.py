db829a0b9ab5a038d3174acbe8e7396bae7fd831
Sushrut Shringarputale <contact@sushshring.com>
2019-09-12 12:29:17 -0400

    Added worker vagrantfiles

--- analysis/perf.py
+++ analysis/perf.py
@@ -2,8 +2,21 @@
 import subprocess
 import sys
 import time
+import csv
 import signal
 from functools import reduce
+
+
+def get_int_from_string(string: str):
+    return int(''.join(c for c in string if c.isdigit()))
+
+
+def parse_bytes(byte_string: str):
+    if 'K' in byte_string:
+        return get_int_from_string(byte_string) * (2**10)
+    elif 'M' in byte_string:
+        return get_int_from_string(byte_string) * (2**20)
+    return int(byte_string)
 
 
 class perf_details(object):
@@ -33,8 +46,8 @@ class perf_details(object):
             memfree = memfile.readline()
             return int(memtotal.split()[1]) - int(memfree.split()[1])
 
-    def __str__(self):
-        return "cpu load: %f, cpu usage: %f, memory usage %d" % (self._cpu_load, self._cpu_usage, self._memory_usage)
+    def __repr__(self):
+        return "cpu load: %f, cpu usage: %f, memory usage %d" % (self._cpu_load, self._cpu_usage,self._memory_usage)
 
 perfs = []
 
@@ -44,18 +57,44 @@ def Average(lst):
         return None
     return reduce(lambda a, b: a + b, lst) / len(lst)
 
+virttopproc: subprocess.Popen = None
+# iostats = open('io_output', 'w')
+def _update_io_usage():
+    io_cmd = "virt-top --script --block-in-bytes --no-csv-cpu --no-csv-mem --csv io_output"# | tail -1 | awk '{print $3\",\"$4\",\"$5\",\"$6}'"
+    global virttopproc
+    virttopproc = subprocess.Popen(io_cmd, shell=True)
+        # return sum([parse_bytes(r) for r in response.split(',')])
+    pass
+
 
 def exit_handler(sig, frame):
     cpu_load_avg = perfs[-1]._cpu_load
     cpu_usage = Average(list(map(lambda x: x._cpu_usage, perfs)))
     mem_usage = Average(list(map(lambda x: x._memory_usage, perfs)))
-    print("cpu load: %f, cpu usage: %f, memory usage %d" % (cpu_load_avg, cpu_usage, mem_usage))
+    io_usage = 0
+    virttopproc.kill()
+    # iostats.flush()
+    with open('io_output', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        relevant_rows = [row for row in reader if "target" in row["Domain name"]]
+        print(relevant_rows)
+        for row in relevant_rows:
+            io_usage += sum([int(r) for r in [row["Block RDBY"], row["Block WRBY"], row["Net RXBY"], row["Net TXBY"]]])
+        csvfile.truncate(0)
+
+    # response = subprocess.check_output("cat io_output | awk '{print $3\",\"$4\",\"$5\",\"$6}'; rm io_output", shell=True).decode()
+    # print(response)
+    # for i in response.split('\n'):
+        # print(i)
+        # io_usage += sum([parse_bytes(r) for r in i.split(',')])
+    print("cpu load: %f, cpu usage: %f, memory usage %d, io_bytes: %d" % (cpu_load_avg, cpu_usage, mem_usage, io_usage))
     sys.exit(0)
 
 
 def get_perf_details():
+    _update_io_usage()
     while True:
-        time.sleep(1)
+        # time.sleep(1)
         perfs.append(perf_details())
 
 if __name__ == '__main__':
